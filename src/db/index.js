04bc6df2f2f1e855146fe4b7867bce1450d770f5
Aditya <adityakumarjha.276@gmail.com>
2025-01-22 22:58:35 +0530

    initilize postgres

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  const client = new Client({
+    user: process.env.PG_USER, 
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE, 
+    password: process.env.PG_PASSWORD, 
+    port: process.env.PG_PORT,
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL:", process.env.PG_HOST);
+    return client;
+  } catch (error) {
+    console.error("Error connecting to PostgreSQL:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
