7cdc23d2cf745b114963fb62ecb797184774a836
Aditya <adityakumarjha.276@gmail.com>
2025-01-26 23:15:56 +0530

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cookieparser from 'cookie-parser';
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://optishrink.vercel.app",
     credentials: true,
 };
 
